9fa89746b5ea599d6794fffa2bfd7f0cfb4095dc
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-20 12:44:13 +0200

    Add working server.js

--- server.js
+++ server.js
@@ -3,3 +3,25 @@ var app = express();
 
 app.set('view engine', 'pug');
 app.set('views','./views');
+
+app.get('/', function(req, res){
+    res.render('login');
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('mainpage', {
+      login: req.query.login,
+      password: req.query.password
+    });
+});
+
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
